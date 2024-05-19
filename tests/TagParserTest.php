449366c93f340c40fb6613c625afe20d9a93ba1e
Patrick Maciel <patrickmaciel.info@gmail.com>
2024-05-18 22:49:02 -0300

    feat: add setup method to our first test

--- tests/TagParserTest.php
+++ tests/TagParserTest.php
@@ -5,11 +5,16 @@ use PHPUnit\Framework\TestCase;
 
 class TagParserTest extends TestCase
 {
+    protected TagParser $parser;
+
+    protected function setup(): void
+    {
+        $this->parser = new TagParser();
+    }
+
     public function test_it_a_single_tag()
     {
-        $parser = new TagParser();
-
-        $result = $parser->parse('personal');
+        $result = $this->parser->parse('personal');
         $expected = ['personal'];
 
         $this->assertEquals($expected, $result);
@@ -17,9 +22,7 @@ class TagParserTest extends TestCase
 
     public function test_it_parsers_a_comma_separated_list_of_tags()
     {
-        $parser = new TagParser();
-
-        $result = $parser->parse('personal, money, food');
+        $result = $this->parser->parse('personal, money, food');
         $expected = ['personal', 'money', 'food'];
 
         $this->assertSame($expected, $result);
@@ -27,22 +30,18 @@ class TagParserTest extends TestCase
 
     public function test_it_spaces_optional()
     {
-        $parser = new TagParser();
-
-        $result = $parser->parse('personal,money,food');
+        $result = $this->parser->parse('personal,money,food');
         $expected = ['personal', 'money', 'food'];
         $this->assertSame($expected, $result);
 
-        $result = $parser->parse('personal|money|food');
+        $result = $this->parser->parse('personal|money|food');
         $expected = ['personal', 'money', 'food'];
         $this->assertSame($expected, $result);
     }
 
     public function test_it_parsers_a_pipe_separted_list_of_tags()
     {
-        $parser = new TagParser();
-
-        $result = $parser->parse('personal | money | food');
+        $result = $this->parser->parse('personal | money | food');
         $expected = ['personal', 'money', 'food'];
 
         $this->assertSame($expected, $result);
